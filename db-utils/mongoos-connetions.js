bd9b055a7af5e73ae3148ca8fbe94df398373b66
prasanth-raj-sankar <prasanthraj0910@gmail.com>
2024-09-14 16:25:29 +0530

    news

--- db-utils/mongoos-connetions.js
+++ db-utils/mongoos-connetions.js
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 
-const host="127.0.0.1:27017";
+// const host="127.0.0.1:27017";
 const dbname="task-35";
 
-const localdb = `mongodb://${host}/${dbname}`;
+// const localdb = `mongodb://${host}/${dbname}`;
+const localdb = "mongodb://127.0.0.1:27017";
 
 export const connectViaMongoose = async () => {
     try{
-        await mongoose.connect(localdb);
+        await mongoose.connect(`${localdb}/${dbname}`);
         console.log("connect to db via mongoose")
     }catch (e){
         console.log("error in connect to Db", e);
