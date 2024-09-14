1627759da3b166496af6b510cf3b907fa2d65cce
prasanth-raj-sankar <prasanthraj0910@gmail.com>
2024-09-14 12:45:45 +0530

    new-14-10-24

--- db-utils/mongoos-connetions.js
+++ db-utils/mongoos-connetions.js
@@ -1,18 +1,41 @@
+// import mongoose from "mongoose";
+
+// const host="127.0.0.1:27017";
+// const dbname="task-35";
+
+// const localdb = `mongodb://${host}/${dbname}`;
+
+// export const connectViaMongoose = async () => {
+//     try{
+//         await mongoose.connect(localdb);
+//         console.log("connect to db via mongoose")
+//     }catch (e){
+//         console.log("error in connect to Db", e);
+//         process.exit(1);
+//     }
+// };
+
+
+
 import mongoose from "mongoose";
 
-const host="127.0.0.1:27017";
-const dbname="task-35";
-
+const host = "127.0.0.1:27017";  // Change this if using a cloud DB
+const dbname = "task-35";
 const localdb = `mongodb://${host}/${dbname}`;
 
-export const connectViaMongoose = async () => {
-    try{
-        await mongoose.connect(localdb);
-        console.log("connect to db via mongoose")
-    }catch (e){
-        console.log("error in connect to Db", e);
-        process.exit(1);
-    }
-};
+// Replace localdb with cloudDb if you are using MongoDB Atlas or similar services
+// const cloudDb = "mongodb+srv://username:password@cluster.mongodb.net/task-35?retryWrites=true&w=majority";
 
+export const connectViaMongoose = async () => {
+  try {
+    await mongoose.connect(localdb, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to DB via Mongoose");
+  } catch (e) {
+    console.log("Error in connecting to DB", e);
+    process.exit(1);
+  }
+};
 
